7594460a0c353cd877888b576ced5cea20b32d4e
IrinaZhuravlyova <79202127+IrinaZhuravlyova@users.noreply.github.com>
2021-02-19 22:59:18 +0300

    QuickSort is added.

--- zhuravleva/homeWork-1.js
+++ zhuravleva/homeWork-1.js
@@ -1,3 +1,4 @@
+// Task 1. Palindrome
 const palindrome = function (str) {
     let newStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase().split(' ');
     let str1 = newStr.join('');
@@ -5,3 +6,47 @@ const palindrome = function (str) {
 
     return str1 === str2;
 }
+
+function swap(items, firstInd, secondInd) {
+    const temp = items[firstInd];
+    items[firstInd] = items[secondInd];
+    items[secondInd] = temp;
+
+}
+// Task 2. Quick Sort
+function partition(items, l, r) {
+    const pivot = items[Math.floor((l + r) / 2)];
+    while (l <= r) {
+        while (items[l] < pivot) {
+            l++;
+        };
+        while (items[r] > pivot) {
+            r--;
+        }
+        if (l <= r) {
+            swap(items, l, r);
+            l++;
+            r--;
+        }
+    }
+    return l;
+}
+
+function quickSort(items, l, r) {
+    let index;
+    debugger
+    if (items.length > 1) {
+        index = partition(items, l, r);
+        
+        if (l < index - 1) {
+            quickSort(items, l, index - 1);
+        }
+        if (index < r) {
+            quickSort(items, index, r);
+        }
+    }
+    return items;
+}
+
+// console.log(quickSort([4, 3, 2, 1, 5, 9, 6, 7], 0, 7));
+
